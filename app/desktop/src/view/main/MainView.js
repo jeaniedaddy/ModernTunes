c3717bac46079a82f7a5bee6d384e0dfae04d99a
No <GLS06383@hke.local>
2020-05-21 14:28:44 -0400

    first commit

---
+++ app/desktop/src/view/main/MainView.js
@@ -0,0 +1,29 @@
+Ext.define('ModernTunes.view.main.MainView', {
+  extend: 'Ext.tab.Panel',
+  xtype: 'mainview',
+  requires: [
+    'ModernTunes.view.main.MainViewController',
+    'ModernTunes.view.main.MainViewModel',
+    'ModernTunes.view.TunesView',
+    'ModernTunes.view.TunesGrid'
+  ],
+  controller: 'mainviewcontroller',
+  viewModel: {
+    type: 'mainviewmodel'
+  },
+  tabBarPosition: 'bottom',
+  items: [{ 
+        title: "thumbnails",
+        xtype: 'tunesview',
+        bind: {
+          store: '{tunes}'
+        }
+    },
+    {
+      title: "Grid",
+      xtype: 'tunesgrid',
+      bind: {
+          store: '{tunes}'
+      }
+    }]
+});
